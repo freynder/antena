8eff9a02e5a6e8abd77e9ca450729a6df5604442
lachrist <lachrist@vub.ac.be>
2018-06-20 16:38:28 +0200

    minor bug

--- node/child.js
+++ node/child.js
@@ -3,7 +3,7 @@ const Request = require("./request.js");
 const empty = Buffer.from("");
 process.on("message", ([channel, secure, options, body]) => {
   Request(secure, options, body, (error, response) => {
-    Fs.writeFile(channel+".res", JSON.stringify([error?error.message:null, JSON.stringify(response)]), "utf8", (error) => {
+    Fs.writeFile(channel+".res", JSON.stringify([error?error.message:null, response]), "utf8", (error) => {
       if (error)
         throw error;
       Fs.writeFile(channel+".sem", empty, (error) => {
