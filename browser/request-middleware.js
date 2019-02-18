77fda874bc699795a1a1e6eeb92d5d398fab0644
Laurent Christophe <lachrist@vub.ac.be>
2019-02-18 17:17:01 +0100

    mime content type

--- browser/request-middleware.js
+++ browser/request-middleware.js
@@ -6,7 +6,7 @@ const dispatch = (receptor, head, body, response) => {
     receptor.onpush(head.substring(1), body);
     response.end();
   } else if (head[0] === "~") {
-    receptor.onpull(head.substring(1), body, (body) => { response.end(body) });
+    receptor.onpull(head.substring(1), body, (body) => { response.end(body, "utf8") });
   } else {
     throw new Error("Request head should start with either '-' or '~', got: "+head);
   }
@@ -17,6 +17,7 @@ module.exports = function (splitter = "__antena__") {
     let head = Split(request.url, splitter);
     if (!head)
       return (next && next(), false);
+    response.setHeader("Content-Type", "text/plain; charset=utf-8");
     if (request.headers["content-length"]) {
       let body = "";
       request.on("data", (data) => { body += data });
